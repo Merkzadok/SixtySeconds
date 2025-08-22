1c6332c9f77c85237b842066a6ef13c389fb0aba
25LP8050 25LP8050 <25LP8050@admins-iMac.local>
2025-08-22 09:36:37 +0800

    img

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["via.placeholder.com"],
+    domains: ["via.placeholder.com"], // энд заавал нэмэх хэрэгтэй
   },
 };
 
--- src/app/(main)/profile/components/ProfileCard.tsx
+++ src/app/(main)/profile/components/ProfileCard.tsx
@@ -4,13 +4,14 @@ export const ProfileCard = () => {
   return (
     <div className="flex items-center gap-4 bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 p-6 rounded-2xl shadow-lg text-white max-w max-auto">
       {/* Профайл зураг */}
-      <Image
-        src="https://via.placeholder.com/80"
-        alt="Аватар"
-        width={80}
-        height={80}
-        className="rounded-full border-4 border-white shadow-lg object-cover"
-      />
+<Image
+  src="https://via.placeholder.com/80"
+  alt="Аватар"
+  width={80}
+  height={80}
+  className="rounded-full border-4 border-white shadow-lg object-cover"
+/>
+
 
       {/* Мэдээлэл */}
       <div>
