7fb052a3c9d40607340dca19f64366a1cfab28ab
25LP8050 25LP8050 <25LP8050@admins-iMac.local>
2025-08-26 11:49:55 +0800

    h

--- src/app/(leaderboard)/leaderboard/page.tsx
+++ src/app/(leaderboard)/leaderboard/page.tsx
@@ -4,7 +4,7 @@ import ReadingStats from "@/app/(leaderboard)/components/ReadingStats";
 
 export default function LeaderboardSectionn() {
   return (
-    <div className="bg-gradient-to-br from-yellow-50 via-orange-50 to-red-50 min-h-screen">
+    <div className="min-h-screen">
       <MainHeader />
         <Leaderboard />
     </div>
