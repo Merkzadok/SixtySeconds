b4113a2569f737070a9811ea6459d3e54b08478b
25LP9262 25LP9262 <25LP9262@admins-iMac.local>
2025-08-28 10:33:18 +0800

    12

--- src/app/(leaderboard)/components/leaderboard.tsx
+++ src/app/(leaderboard)/components/leaderboard.tsx
@@ -41,7 +41,7 @@ const LeaderBoard = () => {
       <div className="max-w-3xl mx-auto space-y-8">
         <ReadingStats />
         <div className="rounded-2xl p-6 shadow-md  ">
-          <h2 className="text-xl font-semibold mb-4. font-bold bg-gradient-to-r from-[#2a7b9b] via-[#57c785] to-[#eddd53] bg-clip-text text-transparent mb-6">
+          <h2 className="text-xl  mb-4. font-bold bg-gradient-to-r from-[#2a7b9b] via-[#57c785] to-[#eddd53] bg-clip-text text-transparent mb-6">
             Шилдэг суралцагчид
           </h2>
           <div className="space-y-4">
