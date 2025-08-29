4ae2ea70f0236166cd9ebc917be25d033bef1797
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-08-29 09:58:28 +0800

    g

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import TransitionLayout from "./auth-animation";
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <TransitionLayout>{children}</TransitionLayout>;
-}
