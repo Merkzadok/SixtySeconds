ff8a1551beb3f62a09c475b6dc020bad77f80e6a
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-09-02 10:00:08 +0800

    a

--- src/app/(main)/reading/page.tsx
+++ src/app/(main)/reading/page.tsx
@@ -1,70 +1,3 @@
-// "use client";
-// import { Button } from "@/Components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/Components/ui/card";
-// import Link from "next/link";
-
-// const level = [
-//   {
-//     title: "Түвшин 1",
-//     description: "1 дүгээр ангийн хүүхдүүдэд амар сэдэв",
-//     path: "/reading/levelOne",
-//   },
-//   {
-//     title: "Түвшин 2",
-//     description: "1 дүгээр ангийн хүүхдүүдэд дунд зэргийн сэдэв",
-//     path: "/reading/levelTwo",
-//   },
-//   {
-//     title: "Түвшин 3",
-//     description: "1 дүгээр ангийн хүүхдүүдэд хэцүү сэдэв",
-//     path: "/reading/levelThree",
-//   },
-//   {
-//     title: "60 sec",
-//     description: "1 дүгээр ангийн хүүхдүүдэд хэцүү сэдэв",
-//     path: "/reading/reading-timer",
-//   },
-// ];
-
-// const ReadingPage = () => {
-//   return (
-//     <div className="min-h-screen bg-background p-4">
-//       <div className="max-w-6xl mx-auto">
-//         <div className="text-center mb-8 mt-12">
-//           <h1 className="text-4xl font-bold mb-4">Унших</h1>
-//           <p className="text-muted-foreground text-lg">Түвшингээ сонгоорой</p>
-//         </div>
-
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-//           {level.map((route, index) => (
-//             <Link href={route.path} key={index}>
-//               <Card className="cursor-pointer hover:shadow-lg transition-shadow">
-//                 <CardHeader>
-//                   <CardTitle className="text-xl">{route.title}</CardTitle>
-//                   <CardDescription>{route.description}</CardDescription>
-//                 </CardHeader>
-//                 <CardContent>
-//                   <Button
-//                     className="w-full bg-gradient-to-r from-green-500 via-green-600 to-green-700 cursor-pointer
-// "
-//                   >
-//                     Түвшин рүү орох
-//                   </Button>
-//                 </CardContent>
-//               </Card>
-//             </Link>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-
 "use client";
 
 import Link from "next/link";
@@ -77,21 +10,21 @@ const levels = [
     title: "Түвшин 1",
     description: "1 дүгээр ангийн хүүхдүүдэд амар сэдэв",
     path: "/reading/levelOne",
-    image: "/book1.webp",
+    image: "/level1.png",
   },
   {
     id: "levelTwo",
     title: "Түвшин 2",
     description: "1 дүгээр ангийн хүүхдүүдэд дунд зэргийн сэдэв",
     path: "/reading/levelTwo",
-    image: "/book2.jpeg",
+    image: "/level2.png",
   },
   {
     id: "levelThree",
     title: "Түвшин 3",
     description: "1 дүгээр ангийн хүүхдүүдэд хэцүү сэдэв",
     path: "/reading/levelThree",
-    image: "/book3.png",
+    image: "/level3.png",
   },
   {
     id: "readingTimer",
