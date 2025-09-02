dc0d6089d92c06fe307a3ca232c3c953b4856b8d
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-09-02 10:17:44 +0800

    bg-color-for-every-page

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -67,7 +67,7 @@ export default function HomePage() {
 
   return (
     <ProtectedRoute>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex bg-[#C0e6BA] flex-col min-h-screen">
         <main className="flex-grow max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="text-center space-y-8">
             <div
